5a9f51de0d0ac432d9d89784e19c2863f66a58e0
Laetitia Deschamps <name>
2021-07-29 18:02:27 +0200

    request notifications ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\ChatRepository;
+use App\Repository\FriendshipRepository;
+use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="main-home")
      */
-    public function index(): Response
+    public function home(Security $security, FriendshipRepository $friendshipRepository, MessageRepository $messageRepository, ChatRepository $chatRepository): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        /** @var User */
+        $user = $security->getUser();
+        $requests = $friendshipRepository->getFriendshipRequests($user);
+    
+        
+     
+       
+        
+        return $this->render('main/home.html.twig', [
+            'requests'=>$requests
         ]);
     }
 }
