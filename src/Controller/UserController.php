8a6bdc932767e09ce0e78c69ee86ef7e53cc1b47
Laetitia Deschamps <name>
2021-07-29 16:06:57 +0200

    user profile

---
+++ src/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/user", name="user-")
+*/
+class UserController extends AbstractController
+{
+    /** 
+     * @Route("/profile", name="profile")
+     */
+    public function index(): Response
+    {
+
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
