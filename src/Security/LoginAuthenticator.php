7f997ef3d3e4d8a5f5c083a9b2bb63cb1b4b619b
Laetitia Deschamps <name>
2021-07-29 12:38:49 +0200

    login methods

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -51,6 +51,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
+        
         return new RedirectResponse($this->urlGenerator->generate('main-home'));
        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
